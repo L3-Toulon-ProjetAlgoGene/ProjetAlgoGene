66258e3d5b42f733d95fdecbd4873e636076e5b7
atobodel <alexandre-tobo-delaleu@etud.univ-tln.fr>
2018-03-23 19:56:36 +0100

    Ajout du main + module cng

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,14 +1,23 @@
 #include"../inc/Cpopulation.h"
 #include"../inc/Cchemin.h"
 #include "../inc/Cville.h"
+#include "../inc/cng.h"
 #include <iostream>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
   {
-  //int a;
-  cout << "Affiche quelque chose" << endl;
+  Cville Paris, Marseille;
+  Paris.init();
+  Marseille.init();
+  cng_init_window(&argc, argv, "CNG Test", 640, 480);
+  cng_put_pixel(Paris.getcoord('x'), Paris.getcoord('y'));
+  cng_put_pixel(Marseille.getcoord('x'), Marseille.getcoord('y'));
+  cng_current_color(0.6, 0.2, 0.7);
+  cng_line(Paris.getcoord('x'), Paris.getcoord('y'), Marseille.getcoord('x'), Marseille.getcoord('y'));
+  cng_main_loop();
+  cng_destroy_window();
 
   return 0;
   }
