ed2eadd305d80c18b36724f2818641b401ec9610
BOUTEMEUR KELLY <kelly-boutemeur@etud.univ-tln.fr>
2018-03-30 19:14:58 +0200

    mutation

--- src/inc/Cmutation.h
+++ src/inc/Cmutation.h
@@ -1,10 +1,29 @@
 #ifndef _MUTATION
 #define _MUTATION
+#include "Cpopulation.h"
+#include "Cchemin.h"
+#include <iostream>
+
+using namespace std;
+
+class Cmutation{
+  private:
+    int new_valeur;
+    int choix_chemin;
+    int choix_empl;
 
 
-void simulpop(int *);
-void aleatoire(int *, int);
-void Affiche_tab(int *);
+  public:
+    Cmutation();
+    Cmutation(int,int,int);
+    int chemin_aleatoire(Cpopulation &);
+    int valeur_aleatoire();
+    int emplacement_aleatoire(Cchemin &);
+    void Affiche();
+
+
+};
+
 
 
 #endif
