7a9665909f63273d282e761c78fccdb2d6c4c339
atobodel <alexandre-tobo-delaleu@etud.univ-tln.fr>
2018-04-06 19:49:08 +0200

    Modification Chemin et du main avec menu

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,26 +6,28 @@
 
 using namespace std;
 
+void menu(void){
+  cng_current_color(1.0, 1.0, 1.0);
+  cng_draw_string("MENU", 20, 10);
+  cng_draw_string("[a] Test avec 2 villes", 20, 50);
+  cng_swap_screen();
+}
+
 Cville* tab(){
-  static int i = 0;
   Cville* tableau;
   Cville Paris, Marseille;
-  if (i == 0) {
-    Paris.init();
-    Marseille.init();
-  }
+  Paris.init();
+  Marseille.init();
   tableau = new Cville[2];
   tableau[0] = Paris;
   tableau[1] = Marseille;
-  i ++;
   return tableau;
 }
 
-
-void afficher(){
-  Cville* tableau;
+void afficher(Cville* tableau){
+  //  Cville* tableau;
   Cville Paris, Marseille;
-  tableau = tab();
+  //  tableau = tab();
   Paris = tableau[0];
   Marseille = tableau[1];
   cng_put_pixel(Paris.getcoord('x'), Paris.getcoord('y'));
@@ -35,11 +37,22 @@ void afficher(){
   cng_swap_screen();
 }
 
-int main(int argc, char* argv[])
-  {
-  cng_init_window(&argc, argv, "CNG Test", 1280, 1024);
-  cng_display_func(afficher);
+int touche_a(void){
+  Cville* tableau;
+  tableau = tab();
   cng_clear_screen();
+  afficher(tableau);
+  cng_delay(3000);
+  cng_clear_screen();
+  menu();
+}
+
+
+int main(int argc, char* argv[])  {
+  cng_init_window(&argc, argv, "CNG Test", 1280, 1024);
+  cng_display_func(menu);
+  cng_assoc_key(cng_a, touche_a);
+//  cng_clear_screen();
   cng_main_loop();
   cng_destroy_window();
   return 0;
